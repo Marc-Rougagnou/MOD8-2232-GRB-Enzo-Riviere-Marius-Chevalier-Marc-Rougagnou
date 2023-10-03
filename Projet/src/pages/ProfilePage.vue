771715206db4db10f4f89b3692d53cc1bb80aad2
MarcRG <marc.rougagnou@efrei.net>
2023-10-03 12:10:39 -0400

    add back profile

--- Projet/src/pages/ProfilePage.vue
+++ Projet/src/pages/ProfilePage.vue
@@ -9,38 +9,46 @@ const router = useRouter();
 </script>
 
 <template>
-  <section v-if="currentuser.id!=0">
-    <h1>Profile</h1>
-    <p>Username: {{currentuser.username}}</p>
-    <p>Email: {{currentuser.email}}</p>
-    <p>Password: {{currentuser.password}}</p>
-    <p>Id: {{currentuser.id}}</p>
-    </section>
-  <section v-else>
-    <h1>Profile</h1>
-    <p>You are not logged in</p>
-    <RouterLink to="/sign-in" class="link">Sign in</RouterLink>
-  </section>
-
-
-
-
-
+    <main>
+        <section v-if="currentuser.id!=0">
+            <h1>Profile</h1>
+            <p>Username: {{currentuser.username}}</p>
+            <p>Email: {{currentuser.email}}</p>
+            <p>Password: {{currentuser.password}}</p>
+            <p>Id: {{currentuser.id}}</p>
+            </section>
+        <section v-else>
+            <h1>Profile</h1>
+            <p>You are not logged in</p>
+            <RouterLink to="/sign-in" class="link">Sign in</RouterLink>
+        </section>
+    </main>
 </template>
 
 <style scoped>
 body {
   font-family: Arial, sans-serif;
-  background-color: #f4f4f4;
+
   margin: 0;
   padding: 0;
 }
 
 /* Style de la section profil */
+
+main{
+  background-image: url('../../img/backprofile.jpg');
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  min-height: 80vh;
+  margin:0;
+  padding:0;
+}
+
+
 section {
   background-color: white;
   max-width: 600px;
-  margin: 20px auto;
   padding: 20px;
   box-shadow: 0 0 10px orange;
 }
