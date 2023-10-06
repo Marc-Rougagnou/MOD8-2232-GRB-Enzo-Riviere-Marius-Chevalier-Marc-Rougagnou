45acb7a03eed4cc7990262d54ec3f62a49179740
Enzo-Riviere <104198804+Enzo-Riviere@users.noreply.github.com>
2023-10-06 17:39:00 -0400

    added delete functionality

--- Projet/src/pages/EditSheetPage.vue
+++ Projet/src/pages/EditSheetPage.vue
@@ -4,6 +4,8 @@ import { ref } from 'vue'
 import SheetForm from "../components/SheetForm.vue";
 import { useRoute } from 'vue-router'
 import { state } from '../store.js';
+import router from '../router';
+
 
 const edit_btn = ref('Save')
                
@@ -14,9 +16,17 @@ const sheet = ref(state.sheets.find((sheet) => sheet.id === current_sheet_id));
 
 const index = state.sheets.findIndex(s => s.id === sheet.value.id);
 
+function remove(del_sheet){
+
+    state.sheets = state.sheets.filter((cur_sheet) => cur_sheet !== del_sheet)
+
+router.push('/');
+}
+
 </script>
 
 <template>
+    <button id="delete-button" @click="remove(sheet)">Delete</button>
     <SheetForm :sheet_init="sheet" :button_text="edit_btn" @response = "(update_sheet) => state.sheets[index] = update_sheet"></SheetForm>
 </template>
 
