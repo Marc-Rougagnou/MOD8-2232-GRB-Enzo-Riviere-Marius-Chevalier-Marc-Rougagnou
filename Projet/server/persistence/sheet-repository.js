57a8de87452afb7af383494db35078afd23981cf
Enzo-Riviere <104198804+Enzo-Riviere@users.noreply.github.com>
2023-10-14 17:42:59 -0400

    first router sheet and repository

--- Projet/server/persistence/sheet-repository.js
+++ Projet/server/persistence/sheet-repository.js
@@ -17,6 +17,40 @@ function mapSheet(row) {
     }
 }
 
+const createSheet = async (title, group, instrument, difficulty,imageData,id_creator) => {
+    const sheet = {
+      id:0,
+      title: title.trim(),
+      group: group.trim(),
+      instrument: instrument.trim(),
+      difficulty: difficulty.trim(),
+      done: "No",
+      imageData: imageData,
+      id_creator: id_creator,
+    }
+  
+    const query = 'INSERT INTO sheets (title, group_name, instruments, difficulty, done, imageData, id_creator) VALUES (?, ?, ?, ?, ?, ?, ?);'
+
+    const parameters = [sheet.title, sheet.group, sheet.instrument, sheet.difficulty, sheet.done, sheet.imageData, sheet.id_creator]
+    const [result] = await database.execute(query, parameters)
+    sheet.id = result.insertId
+    return sheet
+}
+
+const deleteSheet = async (id) => {
+
+    const query = 'DELETE FROM sheets WHERE id = ?'
+
+    const parameters = [id]
+    const [result] = await database.execute(query, parameters)
+    return result.affectedRows > 0
+}
+
+
+
+
 export default {
     findSheets,
+    createSheet,
+    deleteSheet,
 }
--- Projet/server/routes/v1/sheet.js
+++ Projet/server/routes/v1/sheet.js
@@ -1,5 +1,6 @@
 import sheetRepository from "../../persistence/sheet-repository.js";
 import express from 'express';
+import axios from 'axios';
 
 const router = express.Router();
 
@@ -8,6 +9,38 @@ router.get('/sheets', async (req, res, next) => {
         const sheets = await sheetRepository.findSheets();
         res.json(sheets);
     } catch (error) {
+        next(error);
+    }
+});
+
+
+router.post('/sheets', async (req, res, next) => {
+    try {
+        const title = req.body.title;
+        const group = req.body.group;
+        const instrument = req.body.instrument;
+        const difficulty = req.body.difficulty;
+
+        const sheet = await sheetRepository.createSheet(title, group, instrument, difficulty);
+        res.status(201).json(sheet);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.delete('/sheets/:id', async (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id);
+        const deleted =await sheetRepository.deleteSheet(id);
+        if (deleted) {
+            res.sendStatus(200)
+            // Send 200 response indicating film was successfully deleted
+        } 
+        else {
+            next() // Pass request to next request handler or middleware
+        }
+    } 
+    catch (error) {
         next(error);
     }
 });
